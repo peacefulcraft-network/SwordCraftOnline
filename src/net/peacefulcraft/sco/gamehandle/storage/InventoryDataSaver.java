1630fa604be9990ae6cb804d284217404d82a24f
parsonswy <parsonswy@gmail.com>
2019-06-25 13:28:25 -0400

    Add MySQL base

--- src/net/peacefulcraft/sco/gamehandle/storage/InventoryDataSaver.java
+++ src/net/peacefulcraft/sco/gamehandle/storage/InventoryDataSaver.java
@@ -1,5 +1,8 @@
 package net.peacefulcraft.sco.gamehandle.storage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.UUID;
 
 import org.bukkit.inventory.Inventory;
@@ -7,6 +10,37 @@ import org.bukkit.inventory.Inventory;
 import net.peacefulcraft.sco.inventories.InventoryBase;
 
 public class InventoryDataSaver {
+	
+	private static Connection mysql = null;
+	
+	public InventoryDataSaver() {
+		
+	}
+	
+	public static void onEnable() {
+		String ip = "";
+		String name = "";
+		String username = "";
+		String password = "";
+		String dbUrl = "jdbc:mysql//" + ip + ":3306/" + name;
+		
+		try {
+			mysql = DriverManager.getConnection(dbUrl, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void onDisable() {
+		try {
+			if(mysql != null && mysql.isClosed() != true) {
+				mysql.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	/**
 	 * Store inventory data in persistent storage
