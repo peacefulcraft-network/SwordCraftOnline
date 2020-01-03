69159eaa650b8ee024617eb1072aa984a228d6fc
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2020-01-03 13:54:45 -0500

    Removed unused import

--- src/net/peacefulcraft/sco/inventories/listeners/SwordSkillInventoryOpener.java
+++ src/net/peacefulcraft/sco/inventories/listeners/SwordSkillInventoryOpener.java
@@ -4,7 +4,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
