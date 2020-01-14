5f91f78fd2f3e086f63c5d350eb58efcf10dc0da
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2020-01-14 12:30:45 -0500

    Create DungeonManager.java

---
+++ src/net/peacefulcraft/sco/dungeon/DungeonManager.java
@@ -0,0 +1,10 @@
+package net.peacefulcraft.sco.dungeon;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import net.peacefulcraft.sco.gamehandle.player.SCOPlayer;
+
+public class DungeonManager {
+    private static HashMap<UUID, SCOPlayer> players;
+}
