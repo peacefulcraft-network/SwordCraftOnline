9f0715c6946ffd5d5d87fde2f5c823c82c757641
Parsonswy <parsonswy@gmail.com>
2020-01-14 22:36:42 -0500

    Auto join all players

--- src/net/peacefulcraft/sco/gamehandle/listeners/JoinGameListener.java
+++ src/net/peacefulcraft/sco/gamehandle/listeners/JoinGameListener.java
@@ -1,13 +1,25 @@
 package net.peacefulcraft.sco.gamehandle.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import net.peacefulcraft.sco.SwordCraftOnline;
+import net.peacefulcraft.sco.gamehandle.player.SCOPlayer;
+import net.peacefulcraft.sco.gamehandle.tasks.LoadPlayerInventory;
+import net.peacefulcraft.sco.inventory.InventoryType;
 
 public class JoinGameListener implements Listener
 {
 	@EventHandler
 	public void joinEvent(PlayerJoinEvent e) {
-		//TODO:
+		Player p = e.getPlayer();
+		SwordCraftOnline.getGameManager().joinGame(p);
+		
+		@SuppressWarnings("static-access")
+		SCOPlayer s = SwordCraftOnline.getGameManager().findSCOPlayer(p);
+		(new LoadPlayerInventory(s, InventoryType.ACTIVE_SKILL)).runTaskAsynchronously(SwordCraftOnline.getPluginInstance());
+		// (new LoadPlayerInventory(s, InventoryType.MAIN_INVENTORY)).runTaskAsynchronously(SwordCraftOnline.getPluginInstance());
 	}
 }
