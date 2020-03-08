ec095f9c94d795418ee06240346f96182a1a56b9
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2020-03-08 15:53:43 -0400

    Imported all Effects

---
+++ src/net/peacefulcraft/sco/particles/effect/FlameEffect.java
@@ -0,0 +1,35 @@
+package net.peacefulcraft.sco.particles.effect;
+
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.util.Vector;
+
+import net.peacefulcraft.sco.particles.Effect;
+import net.peacefulcraft.sco.particles.EffectManager;
+import net.peacefulcraft.sco.particles.EffectType;
+import net.peacefulcraft.sco.particles.util.RandomUtils;
+
+public class FlameEffect extends Effect {
+
+    public Particle particle = Particle.FLAME;
+
+    public FlameEffect(EffectManager effectManager) {
+        super(effectManager);
+        type = EffectType.REPEATING;
+        period = 1;
+        iterations = 600;
+    }
+
+    @Override
+    public void onRun() {
+        Location location = getLocation();
+        for (int i = 0; i < 10; i++) {
+            Vector v = RandomUtils.getRandomCircleVector().multiply(RandomUtils.random.nextDouble() * 0.6d);
+            v.setY(RandomUtils.random.nextFloat() * 1.8);
+            location.add(v);
+            display(particle, location);
+            location.subtract(v);
+        }
+    }
+
+}
