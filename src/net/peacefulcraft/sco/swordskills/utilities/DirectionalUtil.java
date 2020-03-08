1ea670417c3edf791a1d5c624a33343cd87b1b78
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2020-03-08 12:49:04 -0400

    Update src/net/peacefulcraft/sco/swordskills/utilities/DirectionalUtil.java
    Co-Authored-By: Parsonswy <parsonswy@gmail.com>

--- src/net/peacefulcraft/sco/swordskills/utilities/DirectionalUtil.java
+++ src/net/peacefulcraft/sco/swordskills/utilities/DirectionalUtil.java
@@ -3,6 +3,7 @@ package net.peacefulcraft.sco.swordskills.utilities;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
