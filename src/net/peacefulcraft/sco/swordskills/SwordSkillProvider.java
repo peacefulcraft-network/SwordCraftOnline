1a38d70920bbcfadfbcc8ea7ba7e317c45ed147c
Parsonswy <parsonswy@gmail.com>
2020-04-04 12:42:19 -0400

    Remove item levels

--- src/net/peacefulcraft/sco/swordskills/SwordSkillProvider.java
+++ src/net/peacefulcraft/sco/swordskills/SwordSkillProvider.java
@@ -9,11 +9,6 @@ import net.peacefulcraft.sco.items.ItemTier;
 public interface SwordSkillProvider {
 
   /**
-   * @return Sword Skill Level
-   */
-  public abstract int level();
-
-  /**
    * @return Sword Skill ItemTier
    */
   public abstract ItemTier getTier();
@@ -47,49 +42,30 @@ public interface SwordSkillProvider {
   /**
    * Checks if a skill with the given attributes exists.
    * @param shortName The short / base name for the skill.
-   * @param level The requested skill level.
    * @param tier The requested skill tier.
    * @return True if a skill matching the requested parameters exists.
    *         False if no such skill exists.
    */
-  public static boolean SkillExists(String shortName, int level, ItemTier tier) {
-    return SwordSkillProvider.SkillExists(SwordSkillProvider.getProviderName(shortName, level, tier));
+  public static boolean SkillExists(String shortName, ItemTier tier) {
+    return SwordSkillProvider.SkillExists(SwordSkillProvider.getProviderName(shortName, tier));
   }
 
   /**
    * Takes skill attributes and generates the ItemIdentifier and SwordSKillProvider's class names.
    * IE: Critical Strike level 1, tier Rare -> CriticalStrikeIItemRare.
    * @param shortName The base name of the skill, IE: Critical Strike.
-   * @param level The desired skill level.
    * @param tier The desired skill tier.
    * @return The name of the ItemIdentifier and SwordSkillProvider classes.
    */
-  public static String getProviderName(String shortName, int level, ItemTier tier) {
-    String roman;
-    switch(level) {
-      case  1:
-        roman = "I";
-      break; case 2:
-        roman = "II";
-      break; case 3:
-        roman = "III";
-      break; case 4:
-        roman = "IV";
-      break; case 5:
-        roman = "V";
-      break; default:
-        roman = "I";
-      break;
-    }
-
+  public static String getProviderName(String shortName, ItemTier tier) {
     // Get item tier with first letter capitalized. IE: RARE -> Rare
     String tierString = tier.toString().substring(0, 1).toUpperCase() + tier.toString().toLowerCase().substring(1);
 
-    return shortName.replaceAll(" ", "") + roman + "Item" + tierString;
+    return shortName.replaceAll(" ", "") + "Item" + tierString;
   }
 
   /**
-   * Use a long-from configuration string, IE CriticalStrike-1-COMMON, to generate a SwordSkillProvider.
+   * Use a long-from configuration string, IE CriticalStrike-COMMON, to generate a SwordSkillProvider.
    * @param configString The long-form configuration string.
    * @return The requested SwordSkillProvider.
    */
@@ -97,10 +73,9 @@ public interface SwordSkillProvider {
     try {
       String parts[] = configString.split("-");
       String name = parts[0].replaceAll(" ", "");
-      int level = Integer.valueOf(parts[1]);
       ItemTier tier = ItemTier.valueOf(parts[2]);
     
-      return SwordSkillProvider.generateProvider(name, level, tier);
+      return SwordSkillProvider.generateProvider(name, tier);
     
     } catch(IndexOutOfBoundsException ex) {
       SwordCraftOnline.logSevere("Attempted to generate SwordSkillProvider with invalid long-form configuration string : " + configString);
@@ -114,12 +89,11 @@ public interface SwordSkillProvider {
   /**
    * Generates a SwordSkillProvider instance when given attributes of a valid SwordSkill.
    * @param shortName The shortname / base name of the skill IE: Critial Strike
-   * @param level The requested skill level
    * @param tier the requested ItemTier
    * @return An instance of the requested SwordSkillprovider
    */
-  public static SwordSkillProvider generateProvider(String shortName, int level, ItemTier tier) throws RuntimeException {
-    return SwordSkillProvider.generateProvider(SwordSkillProvider.getProviderName(shortName, level, tier));
+  public static SwordSkillProvider generateProvider(String shortName, ItemTier tier) throws RuntimeException {
+    return SwordSkillProvider.generateProvider(SwordSkillProvider.getProviderName(shortName, tier));
   }
 
   /**
