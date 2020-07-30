f1ba33c2f34fc68c1aa156b9e4ed6ca4d4464774
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2020-07-30 19:38:03 -0400

    Added Network dependency

---
+++ src/net/peacefulcraft/sco/neuralnetwork/CustomNetwork.java
@@ -0,0 +1,13 @@
+package net.peacefulcraft.sco.neuralnetwork;
+
+import java.util.Map;
+
+import me.alexisevelyn.neuralnetwork.NNBaseEntity;
+
+public class CustomNetwork extends NNBaseEntity {
+    
+    public CustomNetwork(Map<String,Object> map) {
+        super(map);
+    }
+
+}
