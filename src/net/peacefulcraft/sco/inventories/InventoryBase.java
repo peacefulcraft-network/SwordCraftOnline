ebc597dc70c18dbc14de2655883d69613a7dda61
Parsonswy <parsonswy@gmail.com>
2020-08-02 22:14:25 -0400

    New inventory abstracts

--- src/net/peacefulcraft/sco/inventories/InventoryBase.java
+++ src/net/peacefulcraft/sco/inventories/InventoryBase.java
@@ -3,27 +3,44 @@ package net.peacefulcraft.sco.inventories;
 import java.util.Arrays;
 
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import net.peacefulcraft.sco.storage.StorageTaskCallbackTask;
+
 public abstract class InventoryBase{
 	
-	private Player observer;
-		public Player getObserver() { return observer; }
-	
-	private InventoryType type;
-		public InventoryType getType() { return type; }
+	private long inventoryId;
+		public long getInventoryId() { return inventoryId; }
+		public void setInventoryId(long inventoryId) { this.inventoryId = inventoryId; }
+
+	public abstract InventoryType getType();
 		
 	protected Inventory inventory;
 		public Inventory getInventory() { return inventory; }
 		
 		
-	public InventoryBase(Player observer, InventoryType inventoryType){
-		this.observer = observer;
-		this.type = inventoryType;	
+	public InventoryBase(){
+		this.initializeDefaultLoadout();
+	}
+
+	/**
+	 * @param long inventoryId: Database ID of the inventory that is to be loaded.
+	 * @param boolean async: Whether the task should be dispatched to an async worker thread or block the current thread.
+	 */
+	public InventoryBase(long inventoryId, boolean async) {
+	}
+
+	/**
+	 * @param long inventoryId: Database ID of the inventory that is to be loaded.
+	 * @param boolean async: Whether the task should be dispatched to an async worker thread or block the current thread.
+	 * @param StorageTaskCallbackTask callback: A task to execute after the inventory fetch task. Runs on success or failure.
+	 * @param boolean sychrnoizeCallback: Should the callback be scheduled onto the main server thread, or run on the same thread as the fetch job
+	 */
+	public InventoryBase(long inventoryId, boolean async, StorageTaskCallbackTask callback, boolean sychrnoizeCallback) {
+
 	}
 	
 	/**
@@ -41,49 +58,34 @@ public abstract class InventoryBase{
 	 * Save the inventory
 	 */
 	public abstract void saveInventory();
-			
-	/**
-	 * Open inventory for player
-	 */
-	public void openInventory() {
-		observer.openInventory(inventory);
-	}
-	
-	/**
-	 * close inventory for player
-	 */
-	public void closeInventory() {
-		observer.closeInventory();
-		this.saveInventory();
-	};
 	
 	public void fillRow(int row, int amount, ItemStack item) {
-    	for(int i = 8-amount; i<=8; i++) {
-    		inventory.setItem( row * 9 + i, item);
-    	}
-    }
+		for(int i = 8-amount; i<=8; i++) {
+			inventory.setItem( row * 9 + i, item);
+		}
+	}
     
-    public void emptyRow(int row) {
-    	fillRow(row, 1, new ItemStack(Material.AIR));
-    }
-    
-    public void addButton(int row, int col, ItemStack item) {
-    	inventory.setItem(row * 9 + col, item);
-    }
-    
-    public void addButton(int row, int col, ItemStack item, String name, String lore, Boolean hidden) {
-    	inventory.setItem(row * 9 + col, createButtomItem(item, name, lore, hidden)); 
-    }
-    
-    private ItemStack createButtomItem(ItemStack item, String name, String lore, Boolean hidden) {
-        ItemMeta im = item.getItemMeta();
-        im.setDisplayName(name);
-        im.setLore(Arrays.asList(lore));
-        if(hidden == true) {
-        	im.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-        }
-        item.setItemMeta(im);
-        return item;
-    }
+	public void emptyRow(int row) {
+		fillRow(row, 1, new ItemStack(Material.AIR));
+	}
+	
+	public void addButton(int row, int col, ItemStack item) {
+		inventory.setItem(row * 9 + col, item);
+	}
+	
+	public void addButton(int row, int col, ItemStack item, String name, String lore, Boolean hidden) {
+		inventory.setItem(row * 9 + col, createButtomItem(item, name, lore, hidden)); 
+	}
+	
+	private ItemStack createButtomItem(ItemStack item, String name, String lore, Boolean hidden) {
+			ItemMeta im = item.getItemMeta();
+			im.setDisplayName(name);
+			im.setLore(Arrays.asList(lore));
+			if(hidden == true) {
+				im.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+			}
+			item.setItemMeta(im);
+			return item;
+	}
 	
 }
