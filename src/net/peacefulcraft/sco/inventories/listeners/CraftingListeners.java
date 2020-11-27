3c4961ea7c939c21059b4d61ae96f85875f4fe3e
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2020-11-27 18:04:09 -0500

    Update CraftingListeners.java
    Added crafting table override. Opening custom interface

--- src/net/peacefulcraft/sco/inventories/listeners/CraftingListeners.java
+++ src/net/peacefulcraft/sco/inventories/listeners/CraftingListeners.java
@@ -6,11 +6,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
@@ -18,6 +21,7 @@ import de.tr7zw.nbtapi.NBTItem;
 import net.peacefulcraft.sco.SwordCraftOnline;
 import net.peacefulcraft.sco.gamehandle.GameManager;
 import net.peacefulcraft.sco.gamehandle.player.SCOPlayer;
+import net.peacefulcraft.sco.inventories.CraftingInventory;
 import net.peacefulcraft.sco.inventories.crafting.Recipe;
 
 public class CraftingListeners implements Listener {
@@ -71,6 +75,21 @@ public class CraftingListeners implements Listener {
         if(!e.getView().getTitle().equalsIgnoreCase("Crafting Table")) { return; }
 
         clearRecipe(e.getInventory(), p);
+    }
+
+    @EventHandler
+    public void rightClickTable(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        SCOPlayer s = GameManager.findSCOPlayer(p);
+        if(s == null) { return; }
+
+        if(e.getAction() != Action.RIGHT_CLICK_BLOCK) { return; }
+        
+        Block b = e.getClickedBlock();
+        if(!b.getType().equals(Material.CRAFTING_TABLE)) { return; }
+
+        new CraftingInventory(s).openInventory();
+        e.setCancelled(true);
     }
 
     /**
