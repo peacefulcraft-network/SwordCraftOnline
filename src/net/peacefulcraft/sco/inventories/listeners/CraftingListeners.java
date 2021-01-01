9dc26731313a2840b4e62e37441ea691c2c514eb
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2021-01-01 11:31:46 -0500

    Removed commented out code

--- src/net/peacefulcraft/sco/inventories/listeners/CraftingListeners.java
+++ src/net/peacefulcraft/sco/inventories/listeners/CraftingListeners.java
@@ -1,28 +1,16 @@
 package net.peacefulcraft.sco.inventories.listeners;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
-import de.tr7zw.nbtapi.NBTItem;
-import net.peacefulcraft.sco.SwordCraftOnline;
 import net.peacefulcraft.sco.gamehandle.GameManager;
 import net.peacefulcraft.sco.gamehandle.player.SCOPlayer;
 import net.peacefulcraft.sco.inventories.CraftingInventory;
-import net.peacefulcraft.sco.inventories.crafting.Recipe;
 
 public class CraftingListeners implements Listener {
     @EventHandler
