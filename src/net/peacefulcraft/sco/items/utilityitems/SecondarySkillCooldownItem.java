f805458314a3933e8be1a9dcb5c534bda2ba88d5
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2021-01-19 12:19:31 -0500

    Skill trigger items

---
+++ src/net/peacefulcraft/sco/items/utilityitems/SecondarySkillCooldownItem.java
@@ -0,0 +1,69 @@
+package net.peacefulcraft.sco.items.utilityitems;
+
+import java.util.ArrayList;
+
+import org.bukkit.Material;
+
+import net.md_5.bungee.api.ChatColor;
+import net.peacefulcraft.sco.items.ItemIdentifier;
+import net.peacefulcraft.sco.items.ItemTier;
+
+public class SecondarySkillCooldownItem implements ItemIdentifier {
+
+    private int quantity;
+
+    @Override
+    public String getName() {
+        return "Secondary Skill Cooldown";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return ChatColor.RED + "Seconary skill in cooldown";
+    }
+
+    @Override
+    public ArrayList<String> getLore() {
+        return new ArrayList<String>();
+    }
+
+    @Override
+    public Material getMaterial() {
+        return Material.RED_STAINED_GLASS_PANE;
+    }
+
+    @Override
+    public ItemTier[] getAllowedTiers() {
+        return new ItemTier[] { ItemTier.COMMON };
+    }
+
+    @Override
+    public ItemTier getTier() {
+        return ItemTier.COMMON;
+    }
+
+    @Override
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    @Override
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean isDroppable() {
+        return false;
+    }
+
+    @Override
+    public boolean isMovable() {
+        return true;
+    }
+
+    public SecondarySkillCooldownItem(ItemTier tier, Integer quantity) {
+        this.quantity = quantity;
+    }
+    
+}
