dfddc182e9659a324cef0d517ff9b1803d3fc3cb
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2021-01-22 17:38:04 -0500

    Update Trigger.java
    Modified Trigger prechecks

--- src/net/peacefulcraft/sco/swordskills/modules/Trigger.java
+++ src/net/peacefulcraft/sco/swordskills/modules/Trigger.java
@@ -5,8 +5,10 @@ import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import net.peacefulcraft.sco.items.ItemIdentifier;
+import net.peacefulcraft.sco.items.WeaponAttributeHolder;
 import net.peacefulcraft.sco.swordskills.SwordSkill;
 import net.peacefulcraft.sco.swordskills.SwordSkillTrigger;
+import net.peacefulcraft.sco.swordskills.SwordSkillType;
 
 public class Trigger implements SwordSkillModule {
 
@@ -14,8 +16,10 @@ public class Trigger implements SwordSkillModule {
         private void setTriggered(boolean set) { triggered = set; }
         private boolean getTriggered() { return this.triggered; }
 
-    public Trigger() {
+    private SwordSkillType type;
 
+    public Trigger(SwordSkillType type) {
+        this.type = type;
     }
 
     @Override
@@ -35,7 +39,12 @@ public class Trigger implements SwordSkillModule {
             ItemIdentifier identifier = ItemIdentifier.resolveItemIdentifier(evv.getItem());
             if(identifier == null || identifier.getMaterial().equals(Material.AIR)) { return false; }
 
-            if(!identifier.getName().equalsIgnoreCase("Secondary Skill Activated")) { return false; }
+            if(this.type.equals(SwordSkillType.PRIMARY) 
+                && !identifier.getName().equalsIgnoreCase("Primary Skill Activated")) { return false; }
+            if(this.type.equals(SwordSkillType.SECONDARY)
+                && !identifier.getName().equalsIgnoreCase("Secondary Skill Activated")) { return false; }
+            if(this.type.equals(SwordSkillType.SWORD) 
+                && !(identifier instanceof WeaponAttributeHolder)) { return false; }
             return true;
         } else if(!(ev instanceof PlayerInteractEvent) && getTriggered() == true) {
             return true;
