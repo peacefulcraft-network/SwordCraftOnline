318b458c9cf5e67bc3a8ef4b6784d4738d4b42b3
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2021-01-23 19:08:49 -0500

    Blind rage skill

---
+++ src/net/peacefulcraft/sco/items/BlindRageItem.java
@@ -0,0 +1,117 @@
+package net.peacefulcraft.sco.items;
+
+import java.util.ArrayList;
+
+import com.google.gson.JsonObject;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import net.peacefulcraft.sco.swordskills.BlindRageSkill;
+import net.peacefulcraft.sco.swordskills.SwordSkill;
+import net.peacefulcraft.sco.swordskills.SwordSkillCaster;
+import net.peacefulcraft.sco.swordskills.SwordSkillProvider;
+import net.peacefulcraft.sco.swordskills.SwordSkillType;
+
+public class BlindRageItem implements SwordSkillProvider {
+
+    private int quantity;
+    private ItemTier tier;
+
+    public BlindRageItem(ItemTier tier, int quantity) {
+        this.quantity = quantity;
+        this.tier = ItemTier.RARE;
+    }
+
+    @Override
+    public String getName() {
+        return "Blind Rage";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return ItemTier.getTierColor(this.tier) + "Blind Rage";
+    }
+
+    @Override
+    public ArrayList<String> getLore() {
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add(ItemTier.getTierColor(this.tier) + "Fill yourself with rage and");
+        lore.add(ItemTier.getTierColor(this.tier) + "strike down your foes...");
+        lore.add(ItemTier.getTierColor(this.tier) + "May inflict blindness.");
+        lore.add(ItemTier.getTierColor(this.tier) + "Gives: Strength II, Blindness I");
+        lore.add(ItemTier.getTierColor(this.tier) + "Effect time: 15 seconds");
+        return lore;
+    }
+
+    @Override
+    public Material getMaterial() {
+        return Material.REDSTONE_BLOCK;
+    }
+
+    @Override
+    public ItemTier[] getAllowedTiers() {
+        return new ItemTier[] { ItemTier.RARE };
+    }
+
+    @Override
+    public ItemTier getTier() {
+        return this.tier;
+    }
+
+    @Override
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    @Override
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean isDroppable() {
+        return false;
+    }
+
+    @Override
+    public boolean isMovable() {
+        return true;
+    }
+
+    @Override
+    public JsonObject getCustomData() {
+        return new JsonObject();
+    }
+
+    @Override
+    public void setCustomData(JsonObject data) {
+
+    }
+
+    @Override
+    public void parseCustomItemData(ItemStack item) {
+
+    }
+
+    @Override
+    public ItemStack applyCustomItemData(ItemStack item, JsonObject data) {
+        return item;
+    }
+
+    @Override
+    public SwordSkillType getType() {
+        return SwordSkillType.SECONDARY;
+    }
+
+    @Override
+    public SwordSkill registerSwordSkill(SwordSkillCaster caster) {
+        return new BlindRageSkill(caster, (SwordSkillProvider)this);
+    }
+
+    @Override
+    public void setModifiers() {
+
+    }
+    
+}
---
+++ src/net/peacefulcraft/sco/swordskills/BlindRageSkill.java
@@ -0,0 +1,52 @@
+package net.peacefulcraft.sco.swordskills;
+
+import org.bukkit.Material;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import net.peacefulcraft.sco.items.ItemIdentifier;
+import net.peacefulcraft.sco.swordskills.utilities.ModifierUser;
+
+public class BlindRageSkill extends SwordSkill {
+
+    public BlindRageSkill(SwordSkillCaster c, SwordSkillProvider provider) {
+        super(c, provider);
+        
+        this.listenFor(SwordSkillTrigger.PLAYER_INTERACT_RIGHT_CLICK);
+    }
+
+    @Override
+    public boolean skillSignature(Event ev) {
+        return true;
+    }
+
+    @Override
+    public boolean skillPreconditions(Event ev) {
+        PlayerInteractEvent evv = (PlayerInteractEvent)ev;
+        ItemIdentifier identifier = ItemIdentifier.resolveItemIdentifier(evv.getItem());
+        if(identifier == null || identifier.getMaterial().equals(Material.AIR)) { return false; }
+
+        if(!identifier.getName().equalsIgnoreCase("Secondary Skill Activated")) { return false; }
+        return true;
+    }
+
+    @Override
+    public void triggerSkill(Event ev) {
+        ModifierUser mu = (ModifierUser)c;
+        mu.getLivingEntity().addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 2, 15));
+        mu.getLivingEntity().addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 1, 15));
+    }
+
+    @Override
+    public void skillUsed() {
+
+    }
+
+    @Override
+    public void unregisterSkill() {
+
+    }
+    
+}
