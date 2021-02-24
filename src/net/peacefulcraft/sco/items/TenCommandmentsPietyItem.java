1922d0768bc1e42835d0764dab178ba14a1bcb40
BrokenKage <47730240+BrokenKage@users.noreply.github.com>
2021-02-23 19:21:45 -0500

    Ten Commandments Piety skill

---
+++ src/net/peacefulcraft/sco/items/TenCommandmentsPietyItem.java
@@ -0,0 +1,126 @@
+package net.peacefulcraft.sco.items;
+
+import java.util.ArrayList;
+
+import com.google.gson.JsonObject;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+import net.peacefulcraft.sco.items.utilities.Glow;
+import net.peacefulcraft.sco.swordskills.SwordSkill;
+import net.peacefulcraft.sco.swordskills.SwordSkillCaster;
+import net.peacefulcraft.sco.swordskills.SwordSkillProvider;
+import net.peacefulcraft.sco.swordskills.SwordSkillType;
+import net.peacefulcraft.sco.swordskills.TenCommandmentsPietySkill;
+
+public class TenCommandmentsPietyItem implements SwordSkillProvider, EphemeralAttributeHolder {
+
+    private ItemTier tier;
+    private int quantity;
+
+    public TenCommandmentsPietyItem(ItemTier tier, int quantity) {
+        this.tier = ItemTier.GODLIKE;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String getName() {
+        return "Ten Commandments Piety";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return ItemTier.getTierColor(tier) + "Ten Commandments: Piety";
+    }
+
+    @Override
+    public ArrayList<String> getLore() {
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add(ItemTier.getTierColor(tier) + "Become the conduit for Piety.");
+        lore.add(ItemTier.getTierColor(tier) + "Any entity with regeneration");
+        lore.add(ItemTier.getTierColor(tier) + "near the conduit is poisoned.");
+        return lore;
+    }
+
+    @Override
+    public Material getMaterial() {
+        return Material.CACTUS;
+    }
+
+    @Override
+    public ItemTier[] getAllowedTiers() {
+        return new ItemTier[] { ItemTier.GODLIKE };
+    }
+
+    @Override
+    public ItemTier getTier() {
+        return tier;
+    }
+
+    @Override
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean isDroppable() {
+        return false;
+    }
+
+    @Override
+    public boolean isMovable() {
+        return true;
+    }
+
+    @Override
+    public JsonObject getCustomData() {
+        return new JsonObject();
+    }
+
+    @Override
+    public void setCustomData(JsonObject data) {
+
+    }
+
+    @Override
+    public void parseCustomItemData(ItemStack item) {
+
+    }
+
+    @Override
+    public ItemStack applyCustomItemData(ItemStack item, JsonObject data) {
+        return item;
+    }
+
+    @Override
+    public ItemStack applyEphemeralAttributes(ItemStack item) {
+        return Glow.addGlow(item);
+    }
+
+    @Override
+    public void parseEphemeralAttributes(ItemStack item) {
+
+    }
+
+    @Override
+    public SwordSkillType getType() {
+        return SwordSkillType.PASSIVE;
+    }
+
+    @Override
+    public SwordSkill registerSwordSkill(SwordSkillCaster caster) {
+        return new TenCommandmentsPietySkill(caster, (SwordSkillProvider)this);
+    }
+
+    @Override
+    public void setModifiers() {
+
+    }
+    
+}
