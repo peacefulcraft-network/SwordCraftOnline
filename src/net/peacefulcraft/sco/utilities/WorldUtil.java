f95e966e4f7dc20f15cb03be47d49239303d67d1
Jonathan L <47730240+BrokenKage@users.noreply.github.com>
2021-07-25 21:22:55 -0700

    Create WorldUtil.java
    World copy and unload utilities

---
+++ src/net/peacefulcraft/sco/utilities/WorldUtil.java
@@ -0,0 +1,70 @@
+package net.peacefulcraft.sco.utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+
+/**
+ * World copy and unload utilities
+ */
+public class WorldUtil {
+
+    public static void copyWorld(String originalWorld, String newWorldName) {
+        World world = Bukkit.getServer().getWorld(originalWorld);
+        if (world == null) { return; }
+        copyWorld(world, newWorldName);
+    }
+
+    public static void copyWorld(World originalWorld, String newWorldName) {
+        copyFileStructure(originalWorld.getWorldFolder(), new File(Bukkit.getWorldContainer(), newWorldName));
+        new WorldCreator(newWorldName).createWorld();
+    }
+
+    public static boolean unloadWorld(String name) {
+        return unloadWorld(Bukkit.getServer().getWorld(name));
+    }
+    
+    public static boolean unloadWorld(World world) {
+        return world!=null && Bukkit.getServer().unloadWorld(world, false);
+    }
+    
+    private static void copyFileStructure(File source, File target){
+        try {
+            ArrayList<String> ignore = new ArrayList<>(Arrays.asList("uid.dat", "session.lock"));
+            if(!ignore.contains(source.getName())) {
+                if(source.isDirectory()) {
+                    if(!target.exists())
+                        if (!target.mkdirs())
+                            throw new IOException("Couldn't create world directory!");
+                    String files[] = source.list();
+                    for (String file : files) {
+                        File srcFile = new File(source, file);
+                        File destFile = new File(target, file);
+                        copyFileStructure(srcFile, destFile);
+                    }
+                } else {
+                    InputStream in = new FileInputStream(source);
+                    OutputStream out = new FileOutputStream(target);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = in.read(buffer)) > 0)
+                        out.write(buffer, 0, length);
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
